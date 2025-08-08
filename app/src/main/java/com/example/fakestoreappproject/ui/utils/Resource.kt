6731a95c49a0a19cedb5f7643e99694fd34bd275
shorouksaffan <shorouk.saffan2005@gmail.com>
2025-08-08 20:36:03 +0300

    mainactivity and utils

--- app/src/main/java/com/example/fakestoreappproject/ui/utils/Resource.kt
+++ app/src/main/java/com/example/fakestoreappproject/ui/utils/Resource.kt
@@ -1,2 +1,10 @@
 package com.example.fakestoreappproject.ui.utils
 
+sealed class Resource<T>(
+    val data: T? = null,
+    val error: String? = null
+) {
+    class Success<T>(data: T) : Resource<T>(data)
+    class Error<T>(error: String) : Resource<T>(error = error)
+    class Loading<T> : Resource<T>()
+}
