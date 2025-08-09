d0520216ad43418dd15ff93510c305a3acd7fd9b
shorouk saffan <shorouk.saffan2005@gmail.com>
2025-08-09 17:24:47 +0300

    MainActivity.kt

--- app/src/main/java/com/example/fakestoreappproject/ui/MainActivity.kt
+++ app/src/main/java/com/example/fakestoreappproject/ui/MainActivity.kt
@@ -5,7 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import com.example.fakestoreappproject.ui.themes.FakeStoreAppProjectTheme // Note: "theme" not "themes"
+import com.example.fakestoreappproject.ui.themes.FakeStoreAppProjectTheme 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
